ff82b464864e0a16f632da0f3d192d76365152e4
colinboumenot <colin.boumenot@yahoo.com>
2024-11-30 21:08:05 -0800

    Day 1 Complete

--- Solutions/Day1.py
+++ Solutions/Day1.py
@@ -1,2 +1,26 @@
+from collections import Counter
 text = open('Input/Day1.txt', 'r').read().splitlines()
-print(text)
+left, right = [], []
+
+for line in text:
+    l, r = line.split('  ')
+    left.append(int(l))
+    right.append(int(r))
+
+left = sorted(left)
+right = sorted(right)
+total = 0
+
+for x in range(len(left)):
+    total += abs(right[x] - left[x])
+
+print(total)
+
+count = Counter(right)
+
+total = 0
+
+for num in left:
+    total += count[num] * num
+
+print(total)
